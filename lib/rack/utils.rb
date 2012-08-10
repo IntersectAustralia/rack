933f2a2f81a057f4dc6e07b768c4ed8f01138438
Sean McCarthy <sean@clanmccarthy.net>
2012-08-10 11:40:42 +1000

    Backport a fix for generic multipart cases

--- lib/rack/utils.rb
+++ lib/rack/utils.rb
@@ -618,7 +618,7 @@ module Rack
 
               data = {:filename => filename, :type => content_type,
                       :name => name, :tempfile => body, :head => head}
-            elsif !filename && content_type
+            elsif !filename && content_type && body.is_a?(IO)
               body.rewind
 
               # Generic multipart cases, not coming from a form
